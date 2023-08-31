6ca174d1b59a99a93d2237d4a8a2706f0e957dd8
nadhiroov <nadhirov@gmail.com>
2023-08-31 19:19:04 +0700

    export sku

--- app/Controllers/Setting.php
+++ app/Controllers/Setting.php
@@ -5,9 +5,10 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use App\Models\M_point;
 use App\Models\M_product;
+use PhpOffice\PhpSpreadsheet\RichText\RichText;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Writer\Xls;
 use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
 
 class Setting extends BaseController
@@ -58,7 +59,7 @@ class Setting extends BaseController
 
         $spreadsheet->setActiveSheetIndex(0);
 
-       /*  $drawing = new Drawing();
+        /*  $drawing = new Drawing();
         $drawing->setName('Logo');
         $drawing->setDescription('logo');
         $drawing->setPath(base_url('assets/img/ais.png'));
@@ -72,16 +73,18 @@ class Setting extends BaseController
 
         $spreadsheet->getActiveSheet()->setCellValue('A2', 'Toko Susu&Perlengkapan Bayi')
             ->setCellValue('A3', 'Jalan Erlangga 83D, Pasuruan');
+
         $spreadsheet->getActiveSheet()->mergeCells('A2:E2');
         $spreadsheet->getActiveSheet()->mergeCells('A3:E3');
 
         $spreadsheet->getActiveSheet()->getStyle('A2')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_JUSTIFY);
-        $spreadsheet->getActiveSheet()->getStyle('A2')->getAlignment()->setVertical(Alignment::VERTICAL_CENTER);
-
         $spreadsheet->getActiveSheet()->getStyle('A3')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_JUSTIFY);
+
+        $spreadsheet->getActiveSheet()->getStyle('A2')->getAlignment()->setVertical(Alignment::VERTICAL_CENTER);
         $spreadsheet->getActiveSheet()->getStyle('A3')->getAlignment()->setVertical(Alignment::VERTICAL_CENTER);
 
-        /* $richText = new RichText();
+        /* 
+        $richText = new RichText();
         $richText->createText('This invoice is ');
 
         $payable = $richText->createTextRun('payable within thirty days after the end of the month');
@@ -92,33 +95,52 @@ class Setting extends BaseController
         $richText->createText(', unless specified otherwise on the invoice.');
 
         $spreadsheet->getActiveSheet()->getCell('A18')->setValue($richText);
-         */
+        */
 
-        $spreadsheet->getActiveSheet()
+
+        $richText = new RichText();
+        $head = $richText->createText('No.');
+        $head->getFont()->setBold(true);
+        $spreadsheet->getActiveSheet()->getCell('A5')->setValue($richText);
+
+        /* $richText->createText('Barcode')->getFont()->setBold(true);
+        $spreadsheet->getActiveSheet()->getCell('B5')->setValue($richText);
+
+        $richText->createText('Product')->getFont()->setBold(true);
+        $spreadsheet->getActiveSheet()->getCell('C5')->setValue($richText);
+
+        $richText->createText('Stock')->getFont()->setBold(true);
+        $spreadsheet->getActiveSheet()->getCell('D5')->setValue($richText);
+
+        $richText->createText('Price')->getFont()->setBold(true);
+        $spreadsheet->getActiveSheet()->getCell('E5')->setValue($richText); */
+        
+
+        /* $spreadsheet->getActiveSheet()
             ->setCellValue('A5', 'No.')
             ->setCellValue('B5', 'Barcode')
             ->setCellValue('C5', 'Product')
             ->setCellValue('D5', 'Stock')
-            ->setCellValue('E5', 'Price');
+            ->setCellValue('E5', 'Price'); */            
 
-        $column = 5;
+        $column = 6;
         $no = 0;
-        
+
         foreach ($products as $product) {
             $spreadsheet->getActiveSheet()
                 ->setCellValue('A' . $column, ++$no)
                 ->setCellValue('B' . $column, $product['barcode'])
                 ->setCellValue('C' . $column, $product['name'])
                 ->setCellValue('D' . $column, $product['stock'])
-                ->setCellValue('E' . $column, 'Rp. ' . number_format($product['price'], 0,',', '.'));
+                ->setCellValue('E' . $column, 'Rp. ' . number_format($product['price'], 0, ',', '.'));
             $column++;
         }
 
-        $writer = new Xlsx($spreadsheet);
+        $writer = new Xls($spreadsheet);
         $filename = date('Y-m-d-His') . '-products-stock';
 
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment;filename=' . $filename . '.xlsx');
+        header('Content-Disposition: attachment;filename=' . $filename . '.xls');
         header('Cache-Control: max-age=0');
 
         $writer->save('php://output');
